516fcfbe01d6dbf90f7ca839e828e375e78700b5
kier1021 <kierjexter.kj@gmail.com>
2020-05-25 11:38:12 +0800

    Initialized spring files

---
+++ api/src/test/java/com/krsh/logmonitor/LogMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krsh.logmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
